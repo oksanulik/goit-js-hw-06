01697da1af7e11c76a7b80dd286e3fbf66952097
oksanulik <kalko_oksanka@ukr.net>
2023-05-19 23:21:29 +0300

    part2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.getElementById("ingredients");
+
+const ingredientsMarkup = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.classList.add("item");
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+});
+
+ingredientsMarkup.forEach((ingredientEl) => {
+  listEl.append(ingredientEl);
+});
